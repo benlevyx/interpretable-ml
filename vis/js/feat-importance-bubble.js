cab9406eb0607696bd16f0ea2ecfecfa92b732c6
Giulia <giulia.zerbini.@thevisualagency.com>
2020-05-01 17:10:29 +0200

    add bubble vis

---
+++ vis/js/feat-importance-bubble.js
@@ -0,0 +1,129 @@
+/**
+ * This is an object constructor that can be reused as needed
+ * The `export default` before the function is necessary so that this can be
+ * imported into `main.js`
+ *
+ * @param _parentElement  -- The ID (no '#') of the `div` element that this vis goes on
+ * @param _data           -- Data (as javascript object)
+ * @param _config         -- An object containing additional parameters for customization (e.g. 'height', 'width')
+ * @constructor
+ */
+export default function FeatImportanceBubble(_parentElement, _data, _config) {
+  this.parentElement = _parentElement;
+  this.data = _data;
+  this.config = _config;
+
+  this.initVis();
+}
+
+/**
+ * Initialize the basic features of the vis
+ *    - Scales
+ *    - SVG and margin
+ */
+FeatImportanceBubble.prototype.initVis = function () {
+  // Because `this ` is not always accessible in every scope (especially with
+  // d3), we save a reference to `this` in the variable `vis` and use that instead.
+  var vis = this;
+
+  // Utility function to initialize the vis using the correct div element and
+  // configuration settings (from utils.js)
+  // E.g. this sets up the margin and SVG, unpacks the config variable
+  // If there are extra config variables, they can be accessed later using
+  // the syntax `vis.config.<var-name>`
+  initVis(vis);
+
+  // Initialize the scales using new D3 v4 syntax (no more scale.band...)
+//   vis.x = d3.scaleBand().rangeRound([0, vis.width], 0.05).padding(0.05);
+//   vis.y = d3.scaleLinear().range([vis.height, 0]);
+
+  // Set the domains. Can also be done later
+//   vis.x.domain(
+//     vis.data.map(function (d) {
+//       return d.feature;
+//     })
+//   );
+//   vis.y.domain([
+//     0,
+//     d3.max(vis.data, function (d) {
+//       return d.value;
+//     }),
+//   ]);
+
+  // Initialize the axis generating functions
+ 
+
+  vis.diameter = 600;
+//   vis.color = d3.scaleOrdinal(d3.schemeCategory20);
+  vis.bubble = d3.pack(data).size([vis.diameter, vis.diameter]).padding(1.5);
+
+  // Call the function to generate DOM elements
+  vis.renderVis();
+};
+
+/**
+ * Draw the visual elements in the DOM
+ *
+ * This function can be modified as necessary. E.g. any classes to apply
+ * element styling should be added here. I recommend doing selection/filtering
+ * or annotation in a separate function and to keep this function as purely
+ * about drawing the basic marks and axes. That way, we can easily render
+ * versions with and without highlighting, annotation, etc.
+ */
+FeatImportanceBubble.prototype.renderVis = function () {
+  var vis = this;
+
+    vis.svg = d3.select("body")
+        .append("svg")
+        .attr("width", vis.diameter)
+        .attr("height", vis.diameter)
+        .attr("class", "bubble");
+
+  vis.nodes = d3.hierarchy(data).sum(function (d) {
+    return d.value;
+  });
+
+ vis.node = vis.svg
+    .selectAll(".node")
+    .data(vis.bubble(vis.nodes).descendants())
+    .enter()
+    .filter(function (d) {
+      return !d.children;
+    })
+    .append("g")
+    .attr("class", "node")
+    .attr("transform", function (d) {
+      return "translate(" + d.x + "," + d.y + ")";
+    });
+
+    vis.node.append("title").text(function (d) {
+    return d.feature + ": " + d.value;
+  });
+
+//   vis.node
+//     .append("circle")
+//     .attr("r", function (d) {
+//       return d.r;
+//     })
+//     .style("fill", "red");
+
+//   vis.node
+//     .append("text")
+//     .attr("dy", ".2em")
+//     .style("text-anchor", "middle")
+//     .text(function (d) {
+//       return d.data.feature.substring(0, d.r / 3);
+//     })
+//     .attr("class", "labels")
+
+//   vis.node
+//     .append("text")
+//     .attr("dy", "1.3em")
+//     .style("text-anchor", "middle")
+//     .text(function (d) {
+//       return d.data.value;
+//     })
+//     .attr("class", "labels")
+
+//   d3.select(self.frameElement).style("height", diameter + "px");
+};
--- vis/js/main.js
+++ vis/js/main.js
@@ -2,6 +2,7 @@
 // Here we import each component from this folder (no need to add a script tag for each one
 import FeatImportanceVis from './feat-importance-vis.js';
 import FeatMeanVis from "./feat-mean-vis.js";
+import FeatImportanceBubble from './feat-importance-bubble.js';
 
 import {components} from './components.js';
 
@@ -25,6 +26,20 @@ window.visComps = {};
 //   // Call the relevant function
 //   drawFeatureImportanceVis();
 // });
+d3.csv('data/feature_ranking.csv').then(function(featureRanking) {
+  featureRanking.forEach(d => {
+    d.value = +d.value;
+    d.std = +d.std;
+  });
+
+  // Save the data in a global. This is to minimize the number of times we need
+  // to load the data. If it's loaded once, it's accessible by all functions.
+  data.featureRanking = featureRanking;
+
+  // Call the relevant function
+  drawFeatureImportanceBubble();
+});
+
 
 d3.csv('data/feature_means.csv').then(function(featureMeans) {
   var features = featureMeans.map(d => d.feature),
@@ -56,5 +71,22 @@ function drawFeatureImportanceVis() {
   // Then create the vis and store it in the global `visComps` so that it can
   // be accessed in other scopes.
   window.visComps.featImportanceVis = new FeatImportanceVis('feat-importance-vis', data.featureRanking, config);
+}
 
+function drawFeatureImportanceBubble() {
+  // The config can contain manually chosen margins, height, width,
+  // and more. E.g. you can specify the index of a point to highlight,
+  // which is then used in the vis to select/highlight elements
+  var config = {
+    'margin': {
+      'bottom': 90,
+      'left': 40,
+      'top': 10,
+      'right': 10
+    }
+  };
+
+  // Then create the vis and store it in the global `visComps` so that it can
+  // be accessed in other scopes.
+  window.visComps.featImportanceBubble = new FeatImportanceBubble('feat-importance-bubble', data.featureRanking, config);
 }
